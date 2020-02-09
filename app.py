801eab9c26e533605f5ea63280cce48005684c3e
legendsergio@gmail.com <legendsergio@gmail.com>
2020-02-09 01:42:16 +0100

    updated port

--- app.py
+++ app.py
@@ -205,4 +205,5 @@ def project_actions_update(project_id, actions_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
